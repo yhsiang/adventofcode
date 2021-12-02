217da948a82df81a37b97267938d53049b2f29e9
Yuan-Hsiang Cheng <lyforever62@hotmail.com>
2021-12-02 13:14:11 +0800

    add day2 solution

--- 2021/day2/main.go
+++ 2021/day2/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	util "github.com/yhsiang/adventofcode"
 )
@@ -19,5 +22,44 @@ func main() {
 		file = input
 	}
 
-	_ = util.Read(file)
+	data := util.Read(file)
+	// fmt.Print("%v", data)
+	var depth = 0
+	var position = 0
+	for _, d := range data {
+		command := strings.Split(d, " ")
+		// fmt.Printf("%s\n", command[0])
+		n, _ := strconv.Atoi(command[1])
+
+		switch command[0] {
+		case "forward":
+			position += n
+		case "down":
+			depth += n
+		case "up":
+			depth -= n
+		}
+	}
+	fmt.Printf("part1: %d\n", position*depth)
+
+	depth = 0
+	position = 0
+	var aim = 0
+	for _, d := range data {
+		command := strings.Split(d, " ")
+		// fmt.Printf("%s\n", command[0])
+		n, _ := strconv.Atoi(command[1])
+
+		switch command[0] {
+		case "forward":
+			position += n
+			depth += aim * n
+		case "down":
+			aim += n
+		case "up":
+			aim -= n
+		}
+		// fmt.Printf("%d, %d, %d\n", aim, position, depth)
+	}
+	fmt.Printf("part2: %d", position*depth)
 }
