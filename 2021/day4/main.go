d55e0897b1551756084220a6f11ab311be1a4d28
Yuan-Hsiang Cheng <lyforever62@hotmail.com>
2021-12-06 17:01:43 +0800

    use one dimension array solution for day4

--- 2021/day4/main.go
+++ 2021/day4/main.go
@@ -20,147 +20,68 @@ type Tuple struct {
 	Y string
 }
 
-type Card struct {
-	Numbers [][]string
-	Marked  [][]string
+type Board struct {
+	Numbers []string
 	Order   int
+	Remove  bool
 }
 
-func initCard(input string, order int) *Card {
-	var numbers [][]string
-	for _, s := range strings.Split(input, "\n") {
-		var temp []string
-		for _, c := range strings.Split(s, " ") {
+func initBoard(input string, order int) *Board {
+	var nums []string
+	for _, rows := range strings.Split(input, "\n") {
+		for _, c := range strings.Split(rows, " ") {
 			if c != "" {
-				temp = append(temp, c)
+				nums = append(nums, c)
 			}
 		}
-		numbers = append(numbers, temp)
 	}
-	var marked [][]string
-	for _, rows := range numbers {
-		var temp = []string{}
-		for range rows {
-			temp = append(temp, "")
-		}
-		marked = append(marked, temp)
-	}
-	return &Card{
-		Numbers: numbers,
-		Marked:  marked,
+	return &Board{
+		Numbers: nums,
 		Order:   order,
+		Remove:  false,
 	}
 }
 
-func reverse(input [][]string) (output [][]string) {
-	for _, rows := range input {
-		var temp = []string{}
-		// fmt.Printf("%d", len(rows))
-		for range rows {
-			// fmt.Printf("%s\n", c)
-			temp = append(temp, "")
-		}
-		output = append(output, temp)
-	}
-	for i, rows := range input {
-		for j, c := range rows {
-			output[j][i] = c
+func (b *Board) getIndex(num string) int {
+	for i, d := range b.Numbers {
+		if d == num {
+			return i
 		}
 	}
-
-	return output
+	return -1
 }
 
-// debug
-func print(input [][]string) {
-	for _, rows := range input {
-		for _, col := range rows {
-			fmt.Printf("%s ", col)
-		}
-		fmt.Printf("\n")
-	}
+func (b *Board) marked(index int) {
+	b.Numbers[index] = "x"
 }
 
-func (c *Card) mark(input string) {
-	for i, rows := range c.Numbers {
-		for j, col := range rows {
-			if col == input {
-				c.Marked[i][j] = "x"
-			}
-		}
-	}
-}
-
-func (c *Card) sum() int64 {
-	var total int64 = 0
-	for i, rows := range c.Numbers {
-		for j, col := range rows {
-			if c.Marked[i][j] != "x" {
-				n, _ := util.Int64(col)
-				total += n
-			}
-		}
-	}
-	return total
-}
-
-func (c *Card) reset() {
-	var marked [][]string
-	for _, rows := range c.Marked {
-		var temp = []string{}
-		for range rows {
-			temp = append(temp, "")
-		}
-		marked = append(marked, temp)
-	}
-	c.Marked = marked
-}
-
-func (c *Card) isBingo() bool {
-	// horizontal
-	for _, rows := range c.Marked {
-		if strings.Join(rows, "") == "xxxxx" {
-			return true //, fmt.Sprintf("row %d", i)
-		}
-	}
+// only check two lines
+func (b *Board) isBingo(index int) bool {
+	base := index % 5
+	times := index / 5
 	// vertical
-	vcard := reverse(c.Marked)
-	for _, rows := range vcard {
-		if strings.Join(rows, "") == "xxxxx" {
-			return true //, fmt.Sprintf("col %d", i)
-		}
+	var vertical = ""
+	for i, j := base, 0; j < 5; i, j = i+5, j+1 {
+		vertical += b.Numbers[i]
 	}
-	// cross := [][]string{
-	// 	{
-	// 		c.Card[0][0],
-	// 		c.Card[1][1],
-	// 		c.Card[2][2],
-	// 		c.Card[3][3],
-	// 		c.Card[4][4],
-	// 	},
-	// 	{
-	// 		c.Card[0][4],
-	// 		c.Card[1][3],
-	// 		c.Card[2][2],
-	// 		c.Card[3][1],
-	// 		c.Card[4][0],
-	// 	},
-	// }
-	// for _, c := range cross {
-	// 	if strings.Join(c, "") == "xxxxx" {
-	// 		return true
-	// 	}
-	// }
-	return false //, ""
+	// horizontal
+	var horizontal = ""
+	for i := 5 * times; i < times*5+5; i++ {
+		horizontal += b.Numbers[i]
+	}
+
+	return vertical == "xxxxx" || horizontal == "xxxxx"
 }
 
-func exist(nums []int, i int) bool {
-	for _, c := range nums {
-		if i == c {
-			return true
+func (b *Board) sum() int64 {
+	var sum int64 = 0
+	for _, num := range b.Numbers {
+		if num != "x" {
+			n, _ := util.Int64(num)
+			sum += n
 		}
 	}
-	return false
+	return sum
 }
 
 func main() {
@@ -172,52 +93,63 @@ func main() {
 	d := strings.Split(file, "\n\n")
 	numbers := strings.Split(d[0], ",")
 	sample := d[1:]
-	var cards []*Card
-	for i, c := range sample {
-		cards = append(cards, initCard(c, i))
+	var boards []*Board
+	for _, c := range sample {
+		boards = append(boards, initBoard(c, 0))
 	}
 
-	var lastCard *Card
 	var trigger string
+	var lastBoard *Board
 	for _, num := range numbers {
-		if lastCard != nil {
+		if lastBoard != nil {
 			break
 		}
-		for _, card := range cards {
-			card.mark(num)
-			if card.isBingo() {
-				trigger = num
-				lastCard = card
-				break
-			}
-		}
-	}
-
-	n, _ := util.Int64(trigger)
-	fmt.Printf("part1: %d\n", lastCard.sum()*n)
-
-	for _, card := range cards {
-		card.reset()
-	}
-
-	lastCard = nil
-	var removed []int
-	for _, num := range numbers {
-		if len(removed) == len(cards) {
-			break
-		}
-		for _, card := range cards {
-			if exist(removed, card.Order) {
+		for _, board := range boards {
+			index := board.getIndex(num)
+			if index == -1 {
 				continue
 			}
-			card.mark(num)
-			if card.isBingo() {
+			board.marked(index)
+			if board.isBingo(index) {
 				trigger = num
-				lastCard = card
-				removed = append(removed, card.Order)
+				lastBoard = board
+				break
+			}
+
+		}
+	}
+	n, _ := util.Int64(trigger)
+	fmt.Printf("part1: %d\n", lastBoard.sum()*n)
+
+	boards = []*Board{}
+	for i, c := range sample {
+		boards = append(boards, initBoard(c, i))
+	}
+
+	var count = 0
+	for _, num := range numbers {
+		if count == len(boards) {
+			break
+		}
+		for _, board := range boards {
+			if board.Remove {
+				continue
+			}
+			index := board.getIndex(num)
+			if index == -1 {
+				continue
+			}
+			board.marked(index)
+
+			if board.isBingo(index) {
+				trigger = num
+				lastBoard = board
+				board.Remove = true
+				count++
 			}
 		}
 	}
+
 	n, _ = util.Int64(trigger)
-	fmt.Printf("part2: %d\n", lastCard.sum()*n)
+	fmt.Printf("part2: %d\n", lastBoard.sum()*n)
 }
