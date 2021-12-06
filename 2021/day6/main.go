7d77830f31a7566686df5b6e3f1529007c5d82a8
Yuan-Hsiang Cheng <lyforever62@hotmail.com>
2021-12-07 01:37:54 +0800

    improved day6 solution

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -16,7 +16,8 @@ var example string
 var input string
 
 func sim(init_fishes []int64, round int) int {
-	fishes := init_fishes
+	fishes := make([]int64, len(init_fishes))
+	copy(fishes, init_fishes)
 	for i := 1; i <= round; i++ {
 		for i := range fishes {
 			if fishes[i] == 0 {
@@ -30,15 +31,15 @@ func sim(init_fishes []int64, round int) int {
 }
 
 func fastSim(init_fishes []int64, round int) int64 {
+	// [9]int64{} != make([]int64, 9)
 	var fishes = make([]int64, 9)
 	for _, d := range init_fishes {
 		fishes[d] += 1
 	}
 
 	for i := 1; i <= round; i++ {
-		new_fish := append(fishes[1:], fishes[0])
-		new_fish[6] += fishes[0]
-		fishes = new_fish
+		fishes[7] += fishes[0]
+		fishes = append(fishes[1:], fishes[0])
 	}
 
 	return util.Sum(fishes)
@@ -52,6 +53,6 @@ func main() {
 
 	data := util.ToInt64(strings.Split(file, ","))
 
-	fmt.Printf("part1: %d\n", sim(data, 80))
+	fmt.Printf("part1: %d\n", sim(data, 80)) // 210 still works
 	fmt.Printf("part2: %d\n", fastSim(data, 256))
 }
