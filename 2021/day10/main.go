d1543f4abd3e7cacc9c6b8bb85b8a6b2ebeab37c
Yuan-Hsiang Cheng <lyforever62@hotmail.com>
2021-12-10 15:08:33 +0800

    improve day10 solution

--- 2021/day10/main.go
+++ 2021/day10/main.go
@@ -20,26 +20,29 @@ type SyntaxChecker struct {
 	Stack []string
 }
 
-func expectedSymbol(symbol string) string {
-	switch symbol {
-	case "}":
-		return "{"
-	case ")":
-		return "("
-	case ">":
-		return "<"
-	case "]":
-		return "["
-	case "[":
-		return "]"
-	case "<":
-		return ">"
-	case "(":
-		return ")"
-	case "{":
-		return "}"
-	}
-	return ""
+var part1Score = map[string]int{
+	")": 3,
+	"]": 57,
+	"}": 1197,
+	">": 25137,
+}
+
+var part2Score = map[string]int{
+	")": 1,
+	"]": 2,
+	"}": 3,
+	">": 4,
+}
+
+var pairSymbols = map[string]string{
+	")": "(",
+	"]": "[",
+	"}": "{",
+	">": "<",
+	"(": ")",
+	"[": "]",
+	"{": "}",
+	"<": ">",
 }
 
 func (s *SyntaxChecker) add(symbol string) (string, error) {
@@ -49,70 +52,29 @@ func (s *SyntaxChecker) add(symbol string) (string, error) {
 	case "}", ")", "]", ">":
 		lastIdx := len(s.Stack) - 1
 		last := s.Stack[lastIdx]
-		expected := expectedSymbol(symbol)
+		expected := pairSymbols[symbol]
 		if expected == last {
 			s.Stack = s.Stack[:lastIdx]
 		} else {
-			return symbol, fmt.Errorf("expected %s, but found %s instead", expectedSymbol(last), symbol)
+			return symbol, fmt.Errorf("expected %s, but found %s instead", pairSymbols[last], symbol)
 		}
 	}
 	return "", nil
 }
 
-func (s *SyntaxChecker) add2(symbol string) {
-	switch symbol {
-	case "{", "(", "[", "<":
-		s.Stack = append(s.Stack, symbol)
-	case "}", ")", "]", ">":
-		lastIdx := len(s.Stack) - 1
-		last := s.Stack[lastIdx]
-		expected := expectedSymbol(symbol)
-		if expected == last {
-			s.Stack = s.Stack[:lastIdx]
-		}
-	}
-}
-
-var part1Score = map[string]int{
-	")": 3,
-	"]": 57,
-	"}": 1197,
-	">": 25137,
-}
-
-func (s *SyntaxChecker) check(line string) string {
+func (s *SyntaxChecker) check(line string) (string, []string) {
 	for _, symbol := range strings.Split(line, "") {
 		errSymbol, err := s.add(symbol)
 		if err != nil {
-			// errSymbols = append(errSymbols, errSymbol)
-			return errSymbol
+			return errSymbol, nil
 		}
 	}
-	return ""
-}
 
-func (s *SyntaxChecker) check2(line string) []string {
-	for _, symbol := range strings.Split(line, "") {
-		s.add2(symbol)
-	}
 	var completed []string
-	// for _, s := range s.Stack {
 	for i := len(s.Stack) - 1; i >= 0; i-- {
-		completed = append(completed, expectedSymbol(s.Stack[i]))
+		completed = append(completed, pairSymbols[s.Stack[i]])
 	}
-
-	return completed
-}
-
-func (s *SyntaxChecker) reset() {
-	s.Stack = []string{}
-}
-
-var part2Score = map[string]int{
-	")": 1,
-	"]": 2,
-	"}": 3,
-	">": 4,
+	return "", completed
 }
 
 func count(input []string) int {
@@ -131,30 +93,20 @@ func main() {
 
 	lines := util.ByLine(file)
 
-	checker := &SyntaxChecker{}
-	var errSymbols []string
-	var remains []string
+	var part1Scores []int
+	var part2Scores []int
 	for _, line := range lines {
-		symbol := checker.check(line)
-		if symbol != "" {
-			errSymbols = append(errSymbols, symbol)
+		checker := &SyntaxChecker{}
+		s1, s2 := checker.check(line)
+		if s1 != "" {
+			part1Scores = append(part1Scores, part1Score[s1])
 		} else {
-			remains = append(remains, line)
+			part2Scores = append(part2Scores, count(s2))
 		}
-		checker.reset()
 	}
 
-	var sum = 0
-	for _, s := range errSymbols {
-		sum += part1Score[s]
-	}
-	fmt.Printf("part1: %d\n", sum)
-	var scores []int
-	for _, r := range remains {
-		scores = append(scores, count(checker.check2(r)))
-		checker.reset()
-	}
-	sort.Ints(scores)
-	fmt.Printf("part2: %d\n", scores[len(scores)/2])
+	fmt.Printf("part1: %d\n", util.SumInt(part1Scores))
+	sort.Ints(part2Scores)
+	fmt.Printf("part2: %d\n", part2Scores[len(part2Scores)/2])
 
 }
