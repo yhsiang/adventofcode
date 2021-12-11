86287a7c8e6f94faf618b311da1656225fb5173e
Yuan-Hsiang Cheng <lyforever62@hotmail.com>
2021-12-11 14:08:22 +0800

    add 2021 day11 solution

---
+++ 2021/day11/main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"strings"
+
+	util "github.com/yhsiang/adventofcode"
+)
+
+//go:embed example
+var example string
+
+//go:embed input
+var input string
+
+type Octopuses struct {
+	Energies map[string]int
+	Rows     int
+	Cols     int
+}
+
+func initMeasurement(file string) *Octopuses {
+	lines := util.ByLine(file)
+	var energies = make(map[string]int)
+	for i, l := range lines {
+		for j, v := range strings.Split(l, "") {
+			n, _ := util.Int64(v)
+			energies[fmt.Sprintf("%d,%d", i, j)] = int(n)
+		}
+	}
+
+	return &Octopuses{
+		Energies: energies,
+		Rows:     len(lines),
+		Cols:     len(lines[0]),
+	}
+
+}
+
+var coords = [][]int{
+	{0, 1},
+	{0, -1},
+	{1, 0},
+	{-1, 0},
+	{1, 1},
+	{-1, 1},
+	{-1, -1},
+	{1, -1},
+}
+
+func (m *Octopuses) flash(x int, y int, flashed map[string]struct{}) {
+	for _, c := range coords {
+		coord := fmt.Sprintf("%d,%d", x+c[0], y+c[1])
+		if _, ok := m.Energies[coord]; ok {
+			m.Energies[coord] += 1
+			if m.Energies[coord] > 9 {
+				if _, ok := flashed[coord]; !ok {
+					flashed[coord] = struct{}{}
+					m.flash(x+c[0], y+c[1], flashed)
+				}
+			}
+		}
+	}
+}
+
+func (m *Octopuses) model(steps int) int {
+	var flashes int
+	for i := 0; i < steps; i++ {
+		var flashed = make(map[string]struct{})
+		for r := 0; r < m.Rows; r++ {
+			for c := 0; c < m.Cols; c++ {
+				coord := fmt.Sprintf("%d,%d", r, c)
+				m.Energies[coord] += 1
+				if m.Energies[coord] > 9 {
+					if _, ok := flashed[coord]; !ok {
+						flashed[coord] = struct{}{}
+						m.flash(r, c, flashed)
+					}
+				}
+			}
+		}
+
+		for k, energy := range m.Energies {
+			if energy > 9 {
+				flashes++
+				m.Energies[k] = 0
+			}
+		}
+	}
+	return flashes
+}
+
+func (m *Octopuses) findSync() int {
+	var steps int
+	for {
+		var flashed = make(map[string]struct{})
+		for r := 0; r < m.Rows; r++ {
+			for c := 0; c < m.Cols; c++ {
+				coord := fmt.Sprintf("%d,%d", r, c)
+				energy := m.Energies[coord]
+				m.Energies[coord] += 1
+				if energy >= 9 {
+					if _, ok := flashed[coord]; !ok {
+						flashed[coord] = struct{}{}
+						m.flash(r, c, flashed)
+					}
+				}
+			}
+		}
+
+		var flashes int
+		for k, energy := range m.Energies {
+			if energy > 9 {
+				flashes++
+				m.Energies[k] = 0
+			}
+		}
+		if flashes == len(m.Energies) {
+			break
+		}
+		steps++
+	}
+	return steps
+}
+
+func (m *Octopuses) print() {
+	for r := 0; r < m.Rows; r++ {
+		var s string
+		for c := 0; c < m.Cols; c++ {
+			coord := fmt.Sprintf("%d,%d", r, c)
+			s += fmt.Sprintf("%d", m.Energies[coord])
+
+		}
+		fmt.Println(s)
+	}
+	fmt.Println()
+}
+
+func main() {
+	var file = example
+	if len(os.Args) == 2 && os.Args[1] == "input" {
+		file = input
+	}
+
+	m := initMeasurement(file)
+	fmt.Printf("part1: %d\n", m.model(100))
+	m = initMeasurement(file)
+	fmt.Printf("part2: %d\n", m.findSync()+1)
+}
