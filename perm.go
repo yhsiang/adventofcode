3d6eef03c83d224aa49e79fe0d678499bd35ac8a
Yuan-Hsiang Cheng <lyforever62@hotmail.com>
2021-12-15 15:55:51 +0800

    add perm for 2019 day7

---
+++ perm.go
@@ -0,0 +1,29 @@
+package util
+
+// Perm calls f with each permutation of a.
+func Perm(a []rune, f func([]rune)) {
+	perm(a, f, 0)
+}
+
+// Permute the values at index i to len(a)-1.
+func perm(a []rune, f func([]rune), i int) {
+	if i > len(a) {
+		f(a)
+		return
+	}
+	perm(a, f, i+1)
+	for j := i + 1; j < len(a); j++ {
+		a[i], a[j] = a[j], a[i]
+		perm(a, f, i+1)
+		a[i], a[j] = a[j], a[i]
+	}
+}
+
+func RuneToInt(runes []rune) []int {
+	var strs []string
+	for _, v := range runes {
+		strs = append(strs, string(v))
+	}
+
+	return ToInt(strs)
+}
