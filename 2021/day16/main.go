8e57ede2e3a9c25ab90f12172b6e3788bb29bd00
Yuan-Hsiang Cheng <lyforever62@hotmail.com>
2021-12-16 22:51:26 +0800

    use prefix eval for day16

--- 2021/day16/main.go
+++ 2021/day16/main.go
@@ -28,50 +28,344 @@ func parse(binary string) int {
 }
 
 type Packet struct {
-	Input   string
 	Version int
 	Type_   int
 	Value   int
+	Length  int
+
+	// operator
+	LengthTypeId  int
+	LengthTypeVal int
 }
 
-func decodePacket(packet *Packet) (*Packet, *Packet) {
-	var next = &Packet{}
-	packet.Version = parse(packet.Input[0:3])
-	packet.Type_ = parse(packet.Input[3:6])
+func decodePacket(data string) (*Packet, string) {
+	var packet = &Packet{}
+	packet.Version = parse(data[0:3])
+	packet.Type_ = parse(data[3:6])
 	var remain string
 	switch packet.Type_ {
 	case 4: // literal value
 		x := 6
 		var output string
 		for {
-			output += packet.Input[x+1 : x+5]
-			if string(packet.Input[x]) == "0" {
+			output += data[x+1 : x+5]
+			if string(data[x]) == "0" {
 				break
 			}
 			x += 5
 
 		}
-		remain = packet.Input[x+5:]
+		remain = data[x+5:]
 		packet.Value = parse(output)
+		packet.Length = x + 5
 	default: // operator
-		i := string(packet.Input[6])
+		i := string(data[6])
 		switch i {
 		case "0":
 			x := 7
 			y := x + 15
-			remain = packet.Input[y:]
-
+			remain = data[y:]
+			packet.LengthTypeId = 1
+			packet.Length = 22
+			packet.LengthTypeVal = parse(data[7:22])
 		case "1":
 			x := 7
 			y := x + 11
-			remain = packet.Input[y:]
+			remain = data[y:]
+			packet.LengthTypeId = 2
+			packet.Length = 18
+			packet.LengthTypeVal = parse(data[7:18])
 		}
 	}
 
-	next.Input = remain
+	return packet, remain
 
-	return packet, next
+}
 
+type Stack []*Packet
+
+func (s *Stack) isEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *Stack) pop() (*Packet, bool) {
+	if s.isEmpty() {
+		return nil, false
+	}
+	elem := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return elem, true
+}
+
+func (s *Stack) push(i *Packet) {
+	*s = append(*s, i)
+}
+
+func sumPackets(ps []*Packet) *Packet {
+	var sum = 0
+	var length = 0
+	for _, p := range ps {
+		length += p.Length
+		sum += p.Value
+	}
+
+	return &Packet{
+		Type_:  0,
+		Length: length,
+		Value:  sum,
+	}
+}
+
+func prodPackets(ps []*Packet) *Packet {
+	var prod = 1
+	var length = 0
+	for _, p := range ps {
+		length += p.Length
+		prod *= p.Value
+	}
+
+	return &Packet{
+		Type_:  1,
+		Length: length,
+		Value:  prod,
+	}
+}
+
+func minPackets(ps []*Packet) *Packet {
+	var value = ps[0].Value
+	var length = 0
+	for _, p := range ps {
+		length += p.Length
+		if p.Value < value {
+			value = p.Value
+		}
+	}
+
+	return &Packet{
+		Type_:  2,
+		Length: length,
+		Value:  value,
+	}
+}
+
+func maxPackets(ps []*Packet) *Packet {
+	var value = ps[0].Value
+	var length = 0
+	for _, p := range ps {
+		length += p.Length
+		if p.Value > value {
+			value = p.Value
+		}
+	}
+
+	return &Packet{
+		Type_:  3,
+		Length: length,
+		Value:  value,
+	}
+}
+
+// eq + 1 3 * 2 2
+func count(input []*Packet) int {
+	pointer := len(input) - 1
+	var stack = &Stack{}
+	for pointer >= 0 {
+		p := input[pointer]
+		// fmt.Printf("%d, %+v\n", pointer, p)
+		// stack.print()
+		// fmt.Println()
+		if p.Type_ == 4 {
+			stack.push(p)
+			// stack = append(stack, p.Value)
+		} else { // operator
+			if p.Type_ == 0 {
+				if p.LengthTypeId == 1 {
+					var nums []*Packet
+					var length int
+					for length < p.LengthTypeVal {
+						n, ok := stack.pop()
+						if ok {
+							nums = append(nums, n)
+							length += n.Length
+						}
+
+					}
+					sum := sumPackets(nums)
+					sum.Length += p.Length
+					stack.push(sum)
+				}
+
+				if p.LengthTypeId == 2 {
+					var nums []*Packet
+					for i := 0; i < p.LengthTypeVal; i++ {
+						n, ok := stack.pop()
+						if ok {
+							nums = append(nums, n)
+						}
+					}
+					sum := sumPackets(nums)
+					sum.Length += p.Length
+					stack.push(sum)
+				}
+			}
+
+			if p.Type_ == 1 {
+				if p.LengthTypeId == 1 {
+					var nums []*Packet
+					var length int
+					for length < p.LengthTypeVal {
+						n, ok := stack.pop()
+						if ok {
+							nums = append(nums, n)
+							length += n.Length
+						}
+					}
+					prod := prodPackets(nums)
+					prod.Length += p.Length
+					stack.push(prod)
+				}
+
+				if p.LengthTypeId == 2 {
+					var nums []*Packet
+					for i := 0; i < p.LengthTypeVal; i++ {
+						n, ok := stack.pop()
+						if ok {
+							nums = append(nums, n)
+						}
+					}
+					prod := prodPackets(nums)
+					prod.Length += p.Length
+					stack.push(prod)
+				}
+			}
+
+			if p.Type_ == 2 {
+				if p.LengthTypeId == 1 {
+					var nums []*Packet
+					var length int
+					for length < p.LengthTypeVal {
+						n, ok := stack.pop()
+						if ok {
+							nums = append(nums, n)
+							length += n.Length
+						}
+
+					}
+					min := minPackets(nums)
+					min.Length += p.Length
+					stack.push(min)
+				}
+
+				if p.LengthTypeId == 2 {
+					var nums []*Packet
+					for i := 0; i < p.LengthTypeVal; i++ {
+						n, ok := stack.pop()
+						if ok {
+							nums = append(nums, n)
+						}
+					}
+					min := minPackets(nums)
+					min.Length += p.Length
+					stack.push(min)
+				}
+			}
+
+			if p.Type_ == 3 {
+				if p.LengthTypeId == 1 {
+					var nums []*Packet
+					var length int
+					for length < p.LengthTypeVal {
+						n, ok := stack.pop()
+						if ok {
+							nums = append(nums, n)
+							length += n.Length
+						}
+					}
+					max := maxPackets(nums)
+					max.Length += p.Length
+					stack.push(max)
+				}
+
+				if p.LengthTypeId == 2 {
+					var nums []*Packet
+					for i := 0; i < p.LengthTypeVal; i++ {
+						n, ok := stack.pop()
+						if ok {
+							nums = append(nums, n)
+						}
+					}
+					max := maxPackets(nums)
+					max.Length += p.Length
+					stack.push(max)
+				}
+			}
+
+			if p.Type_ == 5 {
+				a, _ := stack.pop()
+				b, _ := stack.pop()
+				if a.Value > b.Value {
+					stack.push(&Packet{
+						Length: a.Length + b.Length + p.Length,
+						Value:  1,
+					})
+				} else {
+					stack.push(&Packet{
+						Length: a.Length + b.Length + p.Length,
+						Value:  0,
+					})
+				}
+			}
+
+			if p.Type_ == 6 {
+				a, _ := stack.pop()
+				b, _ := stack.pop()
+				if a.Value < b.Value {
+					stack.push(&Packet{
+						Length: a.Length + b.Length + p.Length,
+						Value:  1,
+					})
+				} else {
+					stack.push(&Packet{
+						Length: a.Length + b.Length + p.Length,
+						Value:  0,
+					})
+				}
+			}
+
+			if p.Type_ == 7 {
+				a, _ := stack.pop()
+				b, _ := stack.pop()
+				if a.Value == b.Value {
+					stack.push(&Packet{
+						Length: a.Length + b.Length + p.Length,
+						Value:  1,
+					})
+				} else {
+					stack.push(&Packet{
+						Length: a.Length + b.Length + p.Length,
+						Value:  0,
+					})
+				}
+			}
+		}
+		pointer--
+	}
+
+	p, _ := stack.pop()
+	return p.Value
+	// stack.print()
+}
+
+func (s *Stack) print() {
+	for _, p := range *s {
+		fmt.Printf("%+v\n", p)
+	}
+}
+
+func print(packets []*Packet) {
+	for _, p := range packets {
+		fmt.Printf("%+v\n", p)
+	}
 }
 
 func decode(data string, n int) (int, int) {
@@ -198,20 +492,21 @@ func main() {
 	}
 
 	bs := convert(file)
-	p := &Packet{
-		Input: bs,
-	}
-	temp := p
+	var temp = bs
 	var sum int
 	var cur *Packet
+	var packets []*Packet
 	for {
 		cur, temp = decodePacket(temp)
 		sum += cur.Version
-		if len(temp.Input) < 11 {
+		packets = append(packets, cur)
+		if len(temp) < 11 {
 			break
 		}
 	}
 	fmt.Printf("part1: %d\n", sum)
-	_, value := decode(bs, 0)
-	fmt.Printf("part2: %d\n", value)
+	fmt.Printf("part2: %d\n", count(packets))
+
+	// _, value := decode(bs, 0)
+	// fmt.Printf("part2: %d\n", value)
 }
